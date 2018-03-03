170c2e6206d628fdbbb470f74dfb427e65e3daa8
surrexi <surrexi03@gmail.com>
2018-03-04 01:36:12 +0300

    Customize deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,38 +2,23 @@
 lock "~> 3.10.1"
 
 set :application, "intertobacco"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :repo_url, "https://github.com/surrexi/intertobacco.git"
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+# Ветка по-умолчанию
+set :branch, 'master'
+# Директория для деплоя
+set :deploy_to, '/home/deploy/applications/intertobacco'
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :log_level, :info
+# Копирующиеся файлы и директории (между деплоями)
+set :linked_files, %w{config/database.yml}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/uploads}
 
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
+# Ruby свистелки
+set :rbenv_type, :user
+set :rbenv_ruby, '2.4.2'
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_roles, :all
 
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml", "config/secrets.yml"
-
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
+# А это рекомендуют добавить для приложений, использующих ActiveRecord
+set :puma_init_active_record, true
